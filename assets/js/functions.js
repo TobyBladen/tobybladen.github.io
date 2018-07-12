31849e58e980862b7cedf9f6232fc19ebc3cee48
Toby Bladen <toby@bladen.cc>
2018-07-12 20:41:41 +0100

    Masters projects and launch of Chinese site

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  var rowsToHide = $("#skill-row-3 li, #skill-row-4 li, #skill-row-5 li");
+  var rowsToHide = $("#skill-row-3 li, #skill-row-4 li, #skill-row-5 li, #skill-row-6 li, #skill-row-7 li");
   var rowArr = $.makeArray(rowsToHide);
   $(rowArr).hide();
   $("#see-more-button").show();
